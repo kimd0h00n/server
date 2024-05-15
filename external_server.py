f9cc7e8d773fe6a3a31725d336b92be0bbfe96a4
kimd0h00n <casease@naver.com>
2024-05-16 01:43:57 +0900

    Update external_server.py

--- external_server.py
+++ external_server.py
@@ -36,5 +36,5 @@ def internal_server_error(error):
     return jsonify({'error': 'Internal Server Error', 'status': 500}), 500
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))  # Default port 5000
+    port = int(os.getenv("PORT", 5001))
     app.run(host='0.0.0.0', port=port, debug=True)  # Enable debug for development to get more detailed error logs
